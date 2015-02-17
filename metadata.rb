48747ffcc7424da570389403e9a5592ac66a6f9f
Peter Green <peter.green@aztek-native.com>
2015-02-17 23:24:33 +0000

    Initial commit: skeleton cookbook created by berkshelf.

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'arma3'
+maintainer       'AzTek Native Ltd.'
+maintainer_email 'peter.green@aztek-native.com'
+license          'All rights reserved'
+description      'Installs/Configures arma3'
+long_description 'Installs/Configures arma3'
+version          '0.1.0'
---
+++ recipes/default.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook Name:: arma3
+# Recipe:: default
+#
+# Copyright (C) 2015 AzTek Native Ltd.
+#
+# All rights reserved - Do Not Redistribute
+#
