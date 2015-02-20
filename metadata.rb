c4e3e12aa66b17c7a89c71eae58d74c60d05e8cf
Peter Green <peter.green@aztek-native.com>
2015-02-20 18:06:31 +0000

    Updated license.

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'arma3'
 maintainer       'AzTek Native Ltd.'
 maintainer_email 'peter.green@aztek-native.com'
-license          'All rights reserved'
+license          'Apache 2.0'
 description      'Installs/Configures arma3'
 long_description 'Installs/Configures arma3'
 version          '0.1.0'
